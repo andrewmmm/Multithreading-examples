952c694e12ec41474d5b649a67b1a70c6de4ae78
Andrew Martin <martin_am@wvwc.edu>
2017-03-21 21:48:34 -0400

    Update ProducerRunnable.java

--- Producer-Consumer-Problem/ProducerRunnable.java
+++ Producer-Consumer-Problem/ProducerRunnable.java
@@ -10,11 +10,6 @@ public class ProducerRunnable implements Runnable {
 		this.rand = new Random();
 		this.Buffer = _Buffer;
 		this.size = size;       			
-		/*
-		 * size is the amount of items in the queue which represents the buffer; 
-		 * the buffer is thought to be fixed at the value of the constant SIZE in main() with size dictating the amount of items in it
-		 * this is a simpler way to implement the buffer in java then to have a fixed capacity int[] and deal with deletions and null elements
-		 */
 	}
 	
 	public void run() {
